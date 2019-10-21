f7748bc0b23dcf844a60dcc471293a1763764175
rhernandez <rhernandez@skyslope.com>
2019-10-20 20:25:20 -0700

    more basic

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var mongojs = require("mongojs");
-
+var exphbs = require("express-handlebars");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var logger = require("morgan");
 
 var app = express();
+var port = process.env.PORT || 3000
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+app.use(express.static("public"));
